2be4599a701528af6a16a0569cb43316d9c4fa25
MaicolRincon931 <139707810+MaicolRincon931@users.noreply.github.com>
2025-04-04 02:08:01 -0500

    Delete src/dist directory

--- src/dist/controllers/AssetController.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from "express";
-export declare class AssetController {
-    static createAsset: (req: Request, res: Response) => Promise<void>;
-    static getAllAsset: (req: Request, res: Response) => Promise<void>;
-    static getAssetById: (req: Request, res: Response) => Promise<void>;
-    static updateAsset: (req: Request, res: Response) => Promise<void>;
-    static deleteAsset: (req: Request, res: Response) => Promise<void>;
-}
